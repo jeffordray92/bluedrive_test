b53dc16add0ee9d633e720ed287924ba3d111f0d
Jefford Ray Mamacus <jeffordraymamacus@gmail.com>
2023-04-15 22:30:00 +0800

    Add models

--- bluedrive_test/settings.py
+++ bluedrive_test/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+
+    'payment',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST Framework Settings
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+is_required = {
+    'blank': False,
+    'null': False
+}
+
+class Currency(models.Model):
+    name = models.CharField(max_length=30)
+    code = models.CharField(max_length=3) # TODO: ADD UNIQUE
+    created_date = models.DateField(auto_now=False, auto_now_add=False)
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reference_code = models.CharField(max_length=10) # TODO: ADD UNIQUE
+    amount = models.FloatField(**is_required)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    is_paid = models.BooleanField(default=False)
+    paid_date = models.DateField()
+    created_date = models.DateField(auto_now=False, auto_now_add=False)
