a3e86ce01836782a8d8733da9170d46e62d47463
Jefford Ray Mamacus <jeffordraymamacus@gmail.com>
2023-04-15 22:17:05 +0800

    Setup DRF project

---
+++ user/views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+
+from rest_framework import viewsets
+
+from user.serializers import UserSerializer
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
